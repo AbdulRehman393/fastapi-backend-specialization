602d6c65aa787a498dc7b2bfb226725897e4a843
AbdulRehmanSaeed <khawajaabdulrehman393@gmail.com>
2026-09-09 17:00:08 +0500

    Add basic GET endpoint with inline execution documentation

--- 01_introduction-to-fastapi-and-backend-development-fundamentals/02_getting_started/01_api_docs.py
+++ 01_introduction-to-fastapi-and-backend-development-fundamentals/02_getting_started/01_api_docs.py
@@ -9,6 +9,11 @@ def get_shipment():
         "status": "in transit"
             }
 
+# Above we have defined a function that simply returns the shipment data and then we used FastAPI
+# to mark this as a route handler on get method for this url i.e., /shipment, We start our server
+# with this command fastapi dev <file_name> on development mode at local host, then we used any 
+# browser to make a request inside of our server and it used our function to return this data back
+# and that's how we define our API endpoints using FastAPI.
 
 
 
