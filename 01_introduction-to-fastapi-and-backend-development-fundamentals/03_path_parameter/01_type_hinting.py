94b4eab7b3207d562026164ff30ae667aa5d3904
AbdulRehmanSaeed <khawajaabdulrehman393@gmail.com>
2026-09-11 17:14:14 +0500

    Learn type hinting conceptws

--- 01_introduction-to-fastapi-and-backend-development-fundamentals/03_path_parameter/01_type_hinting.py
+++ 01_introduction-to-fastapi-and-backend-development-fundamentals/03_path_parameter/01_type_hinting.py
@@ -1,2 +1,59 @@
-num : int = 10
-value : float = 23.43
+from typing import Any
+
+# FastAPI uses Python type hints to understand what data an API expects
+# and what type of data it returns.
+
+
+
+
+name: str = "Abdul Rehman"
+marks: int = 95
+weight: float = 72.5
+
+value: int | float = 12
+
+class City:
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+
+
+# List type hinting
+digits: list[int] = [1, 2, 3, 4, 5]
+
+# Tuple type hinting
+# hinting that the given tuple contains multiples integers
+multpile_3: tuple[int, ...] = (3, 6, 9, 12, 15)
+
+hampshire = City("hampshire", 273829)
+
+
+
+# The main usage of tuple is that we have some fixed values, here we are not only
+# hinting the datatype of each element, we are also hinting on the lenght of the 
+# tuple, which would be only two elements.
+# This is also telling thee first element should be a City object and second elemnet 
+# should be float
+city_temp: tuple[City, float] = (hampshire, 38.6)
+
+# Dictionary type hinting
+shipment: dict[str, str | Any] = {
+    "id": 27361,
+    "weight": 62.3,
+    "content":"wooden table",
+     "status": "in transit"
+
+     }
+
+
+
+def root(num:  float, exp: float | None = .5) -> float:
+    return pow(num, .5)
+
+root_num = root(25)
+
+print(root_num)
+
+
+# To get warning when we provide a differet type of value than expextedm we can use 
+# Mypy Type Checker
