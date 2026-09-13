38d4e05007028ffc7c0426edefac5a448b8588f0
AbdulRehmanSaeed <khawajaabdulrehman393@gmail.com>
2026-09-13 23:39:53 +0500

    Add files via upload

--- 01_introduction-to-fastapi-and-backend-development-fundamentals/03_path_parameter/02_decorator.py
+++ 01_introduction-to-fastapi-and-backend-development-fundamentals/03_path_parameter/02_decorator.py
@@ -1,3 +1,7 @@
+# Decorator = A function that extends the behavior of another function
+#             without modifying the base function
+#             Pass the base function as an argument as an argument to the decorator
+
 
 def custom_fence(fence: str = "+"):
     def add_fence(func):
