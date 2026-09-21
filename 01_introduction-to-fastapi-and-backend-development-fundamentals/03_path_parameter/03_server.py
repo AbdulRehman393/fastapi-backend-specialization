738cbc55f8ef48eb02d22cb5345b283fb1145d05
AbdulRehmanSaeed <khawajaabdulrehman393@gmail.com>
2026-09-21 16:31:26 +0500

    Add files via upload

--- 01_introduction-to-fastapi-and-backend-development-fundamentals/03_path_parameter/03_server.py
+++ 01_introduction-to-fastapi-and-backend-development-fundamentals/03_path_parameter/03_server.py
@@ -1,1 +1,28 @@
+from collections.abc import Callable
+from typing import Any
 
+routes: dict[str, Callable[[Any], None]] = {}
+
+def route(path: str):
+    def register_route(func):
+        routes[path] = func
+        return func
+    return register_route
+
+
+
+@route("/shipment")
+def get_shipment():
+    return "Shipment<1001, in transit"
+
+
+request : str = ""
+
+while request != "quit":
+    request = input(">  ")
+
+    if request in routes:
+        response = routes[request]()
+        print(response, end="\n\n")
+    else:
+        print("Not found")
